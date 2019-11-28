388e37d916618c3959de8fa75c2b23bc8a60ca0b
EC2 Default User <ec2-user@ip-172-31-23-50.us-east-2.compute.internal>
2019-11-28 08:01:55 +0000

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,29 @@
 class UsersController < ApplicationController
+  
+  def show
+    @user = User.find(params[:id])
+  end
+  
   def new
+    @user = User.new
+  end
+  
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "ようこそ Sample Appへ"
+      redirect_to @user
+      #↑ redirect_to user_url(@user)と同じ
+    else
+      render 'new'
+    end
+  end
+
+#プライベートメソッドの定義  
+private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password,
+                    :password_confirmation)
   end
 end
